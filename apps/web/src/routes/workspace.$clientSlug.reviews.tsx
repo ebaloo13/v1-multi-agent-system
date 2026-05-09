dc20c6977f19c15b9fa691cc295aa5e292297ed4
ebaloo <ebocchieriv@gmail.com>
2026-05-08 19:38:51 -0700

    fix: load work items on client reviews route

--- apps/web/src/routes/workspace.$clientSlug.reviews.tsx
+++ apps/web/src/routes/workspace.$clientSlug.reviews.tsx
@@ -1,12 +1,18 @@
 import { createFileRoute } from '@tanstack/react-router'
 import ClientWorkspaceApp from '../components/ClientWorkspaceApp'
+import { getClientWorkItems } from '../lib/client-work-items.functions'
 
 export const Route = createFileRoute('/workspace/$clientSlug/reviews')({
+  loader: ({ params }) =>
+    getClientWorkItems({
+      data: { clientSlug: params.clientSlug },
+    }),
   component: ClientWorkspaceReviewsPage,
 })
 
 function ClientWorkspaceReviewsPage() {
   const { clientSlug } = Route.useParams()
+  const workItems = Route.useLoaderData()
 
-  return <ClientWorkspaceApp clientSlug={clientSlug} view="reviews" />
+  return <ClientWorkspaceApp clientSlug={clientSlug} view="reviews" workItems={workItems} />
 }
